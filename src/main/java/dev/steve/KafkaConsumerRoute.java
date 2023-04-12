851434a3c11fda8b31e04b782f27e081f4782a37
Steve Tran <stran@redhat.com>
2023-04-12 16:20:59 -0400

    Add UUID for group id generation

--- src/main/java/dev/steve/KafkaConsumerRoute.java
+++ src/main/java/dev/steve/KafkaConsumerRoute.java
@@ -2,7 +2,10 @@ package dev.steve;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.kafka.KafkaConstants;
+import org.apache.camel.component.kafka.KafkaEndpoint;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import java.util.UUID;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -17,13 +20,19 @@ public class KafkaConsumerRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        fromF("kafka:%s?brokers=%s&groupId=my-group-id&autoOffsetReset=earliest", "my-topic", kafkaBootstrapServers)
-                .routeId("kafka-consumer-route")
-                .log("${body}")
-                .process(exchange -> {
-                    String key = exchange.getIn().getHeader(KafkaConstants.KEY, String.class);
-                    String value = exchange.getIn().getBody(String.class);
-                    System.out.println("Received message with key: " + key + " and value: " + value);
-                });
+        KafkaEndpoint kafkaEndpoint = getContext().getEndpoint("kafka:my-topic", KafkaEndpoint.class);
+        kafkaEndpoint.getConfiguration().setBrokers(kafkaBootstrapServers);
+        kafkaEndpoint.getConfiguration().setGroupId(UUID.randomUUID().toString()); // generate a random group.id
+        kafkaEndpoint.getConfiguration().setAutoOffsetReset("earliest");
+
+
+        from(kafkaEndpoint)
+            .routeId("kafka-consumer-route")
+            .log("${body}")
+            .process(exchange -> {
+                String key = exchange.getIn().getHeader(KafkaConstants.KEY, String.class);
+                String value = exchange.getIn().getBody(String.class);
+                System.out.println("Received message with key: " + key + " and value: " + value);
+            });
     }
 }
