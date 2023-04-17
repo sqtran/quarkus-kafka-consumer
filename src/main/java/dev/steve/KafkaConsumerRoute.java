dbf843be0375925162e2c94574ce8d1eeadf4c90
Steve Tran <stran@redhat.com>
2023-04-17 13:58:12 -0400

    Add round-robin configuration

--- src/main/java/dev/steve/KafkaConsumerRoute.java
+++ src/main/java/dev/steve/KafkaConsumerRoute.java
@@ -5,7 +5,7 @@ import org.apache.camel.component.kafka.KafkaConstants;
 import org.apache.camel.component.kafka.KafkaEndpoint;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
-import java.util.UUID;
+import java.net.InetAddress;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -22,9 +22,12 @@ public class KafkaConsumerRoute extends RouteBuilder {
 
         KafkaEndpoint kafkaEndpoint = getContext().getEndpoint("kafka:my-topic", KafkaEndpoint.class);
         kafkaEndpoint.getConfiguration().setBrokers(kafkaBootstrapServers);
-        kafkaEndpoint.getConfiguration().setGroupId(UUID.randomUUID().toString()); // generate a random group.id
+        //kafkaEndpoint.getConfiguration().setGroupId(UUID.randomUUID().toString()); // generate a random group.id
+        kafkaEndpoint.getConfiguration().setGroupId("mygroupid");
         kafkaEndpoint.getConfiguration().setAutoOffsetReset("earliest");
-
+        kafkaEndpoint.getConfiguration().setMaxPollRecords(1);
+        kafkaEndpoint.getConfiguration().setPartitionAssignor("org.apache.kafka.clients.consumer.RoundRobinAssignor");
+        kafkaEndpoint.getConfiguration().setClientId(InetAddress.getLocalHost().getHostName());
 
         from(kafkaEndpoint)
             .routeId("kafka-consumer-route")
@@ -32,7 +35,8 @@ public class KafkaConsumerRoute extends RouteBuilder {
             .process(exchange -> {
                 String key = exchange.getIn().getHeader(KafkaConstants.KEY, String.class);
                 String value = exchange.getIn().getBody(String.class);
-                System.out.println("Received message with key: " + key + " and value: " + value);
+                System.out.println("Received message by " + InetAddress.getLocalHost().getHostName() + " with key: " + key + " and value: " + value);
+                Thread.sleep(10000);
             });
     }
 }
